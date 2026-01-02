21ca60ff878a224e8cfd6f4cab826d90d23976f4
Lucas Lima <lucaslimacnt14@gmail.com>
2026-01-02 19:45:48 -0400

    first commit to web app

---
+++ src/main/java/com/eulucaslim/springmongodb/SpringmongodbApplication.java
@@ -0,0 +1,13 @@
+package com.eulucaslim.springmongodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmongodbApplication {
+
+	 static void main(String[] args) {
+		SpringApplication.run(SpringmongodbApplication.class, args);
+	}
+
+}
