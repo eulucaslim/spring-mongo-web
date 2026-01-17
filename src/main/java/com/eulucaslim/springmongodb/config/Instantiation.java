1683908ce6499abe4b4dae10b4cd7f6a6fd6cee6
Lucas Lima <lucaslimacnt14@gmail.com>
2026-01-16 22:17:15 -0300

    Protecao dos dados com DTO

--- src/main/java/com/eulucaslim/springmongodb/config/Instantiation.java
+++ src/main/java/com/eulucaslim/springmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.eulucaslim.springmongodb.config;
 
 import com.eulucaslim.springmongodb.domain.Post;
 import com.eulucaslim.springmongodb.domain.User;
+import com.eulucaslim.springmongodb.dto.AuthorDTO;
 import com.eulucaslim.springmongodb.repositories.PostRepository;
 import com.eulucaslim.springmongodb.repositories.UserRepository;
 import org.springframework.boot.CommandLineRunner;
@@ -36,10 +37,11 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para sao paulo!", maria);
-        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hj!", maria);
-
         userRepository.saveAll(Arrays.asList(maria, alex, bob));
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"), "Partiu Viagem", "Vou viajar para sao paulo!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("23/03/2018"), "Bom dia", "Acordei feliz hj!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1, post2));
     }
 }
