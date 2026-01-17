a02a14d619a2f4897affeea7b0cb8baacfcc2742
Lucas Lima <lucaslimacnt14@gmail.com>
2026-01-17 16:46:13 -0300

    Endpoint to get posts by User

--- src/main/java/com/eulucaslim/springmongodb/resources/UserResource.java
+++ src/main/java/com/eulucaslim/springmongodb/resources/UserResource.java
@@ -1,5 +1,6 @@
 package com.eulucaslim.springmongodb.resources;
 
+import com.eulucaslim.springmongodb.domain.Post;
 import com.eulucaslim.springmongodb.domain.User;
 import com.eulucaslim.springmongodb.dto.UserDTO;
 import com.eulucaslim.springmongodb.mapper.UserMapper;
@@ -58,5 +59,11 @@ public class UserResource {
         User entity = mapper.toEntity(id, dto);
         User userUpdated = service.update(id, entity);
         return ResponseEntity.ok().body(mapper.toDTO(userUpdated));
+    }
+
+    @GetMapping("/{id}/posts")
+    public ResponseEntity<List<Post>> findPosts(@PathVariable String id) {
+        User obj = service.findById(id);
+        return ResponseEntity.ok().body(obj.getPosts());
     }
 }
